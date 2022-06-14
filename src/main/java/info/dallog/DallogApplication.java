224c1107a534fb610d6f679b7a0f96d906d519f8
정현준 <jeongcode@naver.com>
2022-06-14 21:10:56 +0900

    Initial commit

---
+++ src/main/java/info/dallog/DallogApplication.java
@@ -0,0 +1,13 @@
+package info.dallog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DallogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DallogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/dallog/DallogApplicationTests.java
@@ -0,0 +1,13 @@
+package info.dallog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DallogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
